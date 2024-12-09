8a0a9adb90350510989348b564b377db25772d0f
Geo <dmytro.dovzhenko.pm.2022@lpnu.ua>
2024-12-09 21:42:49 +0200

    Add project files.

---
+++ Lab 3.2/Features/Cataas.feature
@@ -0,0 +1,8 @@
+﻿Feature: Redirect to Cat GIF
+  As a user of the Cataas API
+  I want to ensure that the endpoint redirects me to a GIF
+  So that I can confirm the API behavior
+
+  Scenario: Verify redirect to the GIF endpoint
+    Given I send a GET request to the GIF endpoint
+    Then I should be redirected to the GIF site
---
+++ Lab 3.2/StepDefinitions/CatassSteps.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using TechTalk.SpecFlow;
+
+namespace CataasAPITests.StepDefinitions
+{
+    [Binding]
+    public class CataasStepDefinitions
+    {
+        private RestResponse response;
+
+        [Given(@"I send a GET request to the GIF endpoint")]
+        public void GivenISendAGETRequestToTheGIFEndpoint()
+        {
+            var client = new RestClient("https://cataas.com/cat/gif");
+            var request = new RestRequest
+            {
+                Method = Method.Get
+            };
+
+            // Виконуємо запит
+            response = client.Execute(request);
+        }
+
+        [Then(@"I should be redirected to the GIF site")]
+        public void ThenIShouldBeRedirectedToTheGIFSite()
+        {
+            // Перевіряємо статус-код
+            Assert.AreEqual(200, (int)response.StatusCode, $"Unexpected status code: {(int)response.StatusCode}");
+
+            // Перевіряємо фінальну URL-адресу
+            Assert.IsNotNull(response.ResponseUri, "Redirected URL is null");
+            var finalUrl = response.ResponseUri.ToString();
+            Assert.IsTrue(finalUrl.Contains("/cat/gif"), $"Redirected URL is not as expected: {finalUrl}");
+        }
+    }
+
+}
